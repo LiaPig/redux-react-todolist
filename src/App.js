b0e1bc95a5fe4221abcc41d74bd6b786842ff851
ailgip <huangliya@25qp.cn>
2020-09-13 23:01:45 +0800

    引入store的概念

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect, useCallback} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import List from './components/List/index.jsx'
 import Control from './components/Control'
@@ -7,16 +7,16 @@ import reducer from "./reducers";
 
 const LS_KEY = 'LS_TD'
 
+const store = {
+    todos: [],
+    incrementCount: 0
+}
+
 function App() {
     const [todos, setTodos] = useState([]);
     const [incrementCount, setIncrementCount] = useState(0);
 
-    const dispatch = useCallback((action) => {
-        // 将所有的state，封装在一个大的 states 对象里，key、value都为state
-        const states = {
-            todos,
-            incrementCount
-        };
+    const dispatch = (action) => {
         // 将所有 state 的 setter，封装在一个大的 setters 对象里，key 名为 state 的名，value 为对应的 setter
         const setters = {
             todos: setTodos,
@@ -25,17 +25,17 @@ function App() {
 
         // 判断：如果是异步Action，就做以下操作，做完就return，退出函数，不参与后面的同步Action 的操作
         if (typeof action === 'function') {
-            action(dispatch, () => states);
+            action(dispatch, () => store);
             return;
         }
 
         // 根据传入的 action，去调用 reducer 函数，获取到返回的修改后的 states 值
-        const newStates = reducer(states, action);
+        const newStates = reducer(store, action);
         // 循环 states 大对象，更新里面的state
         for (let key in newStates) {
             setters[key](newStates[key]);
         }
-    }, [todos, incrementCount])
+    }
 
     const bindActionCreators = (actionCreators, dispatch) => {
         const result = {};
@@ -61,6 +61,13 @@ function App() {
     useEffect(() => {
         localStorage.setItem(LS_KEY, JSON.stringify(todos));
     }, [todos])
+
+    useEffect(() => {
+        Object.assign(store, {
+            todos,
+            incrementCount
+        })
+    }, [todos, incrementCount])
 
 
     return (
